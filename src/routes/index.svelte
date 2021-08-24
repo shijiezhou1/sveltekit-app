28cc24a258a03483922966e01a0039a5d74965b6
Shijie Zhou <cs5129606@gmail.com>
2021-08-24 07:41:58 +0000

    Setup folder

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,21 +11,6 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
 	<Counter />
 </section>
 
